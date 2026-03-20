1f2e7fe25fab7d29a03c56921f9cc3c04d9c4e9f
AlokPy1484 <alokpandey1483@gmail.com>
2026-03-21 02:54:02 +0530

    contact form complete

--- .next/dev/static/chunks/Desktop_Website_dev_Plex2_0_app_page_tsx_e23dec29._.js
+++ .next/dev/static/chunks/Desktop_Website_dev_Plex2_0_app_page_tsx_e23dec29._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/Desktop_Website_dev_Plex2_0_05e6f08a._.js",
+  "static/chunks/Desktop_Website_dev_Plex2_0_5371e049._.js",
   "static/chunks/ef9cd_next_a2a85115._.js",
   "static/chunks/ef9cd_three_build_three_module_e9991d11.js",
   "static/chunks/ef9cd_tailwind-merge_dist_bundle-mjs_mjs_09010951._.js",
@@ -9,7 +9,7 @@
   "static/chunks/ef9cd_framer-motion_dist_es_1599301a._.js",
   "static/chunks/ef9cd_react-icons_si_index_mjs_58cec96a._.js",
   "static/chunks/ef9cd_react-icons_lib_fb595e05._.js",
-  "static/chunks/ef9cd_05251e00._.js"
+  "static/chunks/ef9cd_aa72276f._.js"
 ],
     source: "dynamic"
 });
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import PickMe from './components/PickMe';
 import ServicesSection from './components/ServicesSection';
 import ShowcaseGrid from './components/ShowcaseGrid';
 import Testimonial from './components/Testimonial';
+import ContactSection from './components/ContactSection';
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
       <LogoLooper/>
       <PickMe/>
       <Testimonial/> 
+      <ContactSection/>
       <Footer/>
    </div>
   );
